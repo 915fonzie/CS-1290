8d6882992377206d0421e72cd85636e46f4de617
915fonzie <39348745+915fonzie@users.noreply.github.com>
2018-05-20 20:48:48 -0600

    Add files via upload

---
+++ Chapter 1 Question 1.2/Ch1Q2.java
@@ -0,0 +1,42 @@
+package cs1290;
+import java.util.*;
+
+public class Ch1Q2 {
+
+	public static void main(String[] args) {
+		String s1 = "abc";
+		String s2 = "cba";
+		
+		System.out.println(permutation(s1, s2));
+
+	}
+	
+	public static boolean permutation(String s1, String s2) {
+		
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		if(s1.length() != s2. length()) {
+			return false;
+		}
+		
+		for(int i= 0; i < s1.length(); i++) {
+			
+			int count1 = map.getOrDefault(s1.charAt(i), 0);
+			int count2 = map.getOrDefault(s2.charAt(i), 0);
+			
+			if(s1.charAt(i) != s2.charAt(i)) {
+			map.put(s1.charAt(i), count1 + 1);
+			map.put(s2.charAt(i), count2 - 1);
+			}
+		}
+		
+		for(Character c: map.keySet()) {
+			if(map.get(c) != 0) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+}
